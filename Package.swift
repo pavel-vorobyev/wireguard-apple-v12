07c1cb6f87da7daf22ff68a170992fc03aacab7e
Pavel Vorobyev <38635444+pavel-vorobyev@users.noreply.github.com>
2023-04-11 03:43:14 +0300

    Update Package.swift
    Signed-off-by: Pavel Vorobyev <38635444+pavel-vorobyev@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "WireGuardKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15)
+        .iOS(.v12)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
